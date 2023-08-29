043e73670b7fb21e01aa1181cd317bea21f9a76b
ismail MISIR <ismmsr@hotmail.com>
2023-08-30 01:12:18 +0300

    3. committim

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 def home():
     return 'Home Page'
 first_name = 'albert'
+sur_name = 'cova32'
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8070, debug=False)
